51d319b1c81ab09473eb366548f47efd82dbad10
felpfsf <feferreira.sf@gmail.com>
2022-05-05 20:22:55 -0300

    aula 4 - html (terminando as seções)  - css (várias alterações)- js (scroll e botao top) -

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
+// a função executa direto na janela, previne erros ocasionais por falta de carregamento da página
+window.addEventListener('scroll', onScroll)
+
+onScroll() // executa uma primeira vez ao carregar a página
+
 function onScroll() {
+  showNavOnScroll()
+  showBacktoTopButton()
+}
+
+function showNavOnScroll() {
   if (scrollY > 0) {
     navigation.classList.add('scroll');
   } else {
     navigation.classList.remove('scroll');
+  }
+}
+
+function showBacktoTopButton() {
+  if (scrollY > 500) {
+    backtoTopButton.classList.add('show');
+  } else {
+    backtoTopButton.classList.remove('show');
   }
 }
 
@@ -13,3 +31,25 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove('menu-expanded');
 }
+
+
+
+// ScrollReveal Lib
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+}).reveal(`
+#home, 
+#home img, 
+#home stats, 
+#services,
+#services header,
+#services .card
+#about, 
+#about header,
+#about .content
+#contact,
+#contact header,
+#contact .content
+`);
